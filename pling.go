1952d91d61a5cc5dab087cd09ecc88158b14a2ca
Orion Osborn <orion.osborn@gmail.com>
2014-12-04 14:49:38 -0700

    implemented in go

---
+++ pling.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "strconv"
+  "fmt"
+  )
+
+func main() {
+  var x int = 28
+  var y int = 1755
+  var z int = 34
+
+  fmt.Println(raindrops(x))
+  fmt.Println(raindrops(y))
+  fmt.Println(raindrops(z))
+}
+
+func raindrops(num int) string {
+  output := ""
+  if pling(num) { output += "Pling" }
+  if plang(num) { output += "Plang" }
+  if plong(num) { output += "Plong" }
+  if !pling(num) && !plang(num) && !plong(num) { output = strconv.Itoa(num) }
+  return output
+}
+
+func pling(num int) bool { return num % 3 == 0 }
+func plang(num int) bool { return num % 5 == 0 }
+func plong(num int) bool { return num % 7 == 0 }
